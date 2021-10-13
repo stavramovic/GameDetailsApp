9b0b64faf6a005159a3a627d0b6ab13fd5b2ceb2
stavramovic <stefan.avramovic096@gmail.com>
2021-10-13 19:05:03 +0200

    minor css improvements

--- src/Components/GameDetails.jsx
+++ src/Components/GameDetails.jsx
@@ -19,17 +19,17 @@ const GameDetails = ({name, backgroundImage, description, relDate, developer, we
                             <div className='descriptionStyle'>
                                 {`${description.substring(0, 500)}...`}  
                             </div>                                                                           
-                            <div className='text-font'>Tags:&nbsp;{tags}</div>
+                            <div className='fontSize'>Tags:&nbsp;{tags}</div>
                         </div>
 
                         <div className='rightSide'>
                             <p className='detailsSize'>Game Details:</p>
                             <div className='devs'>Developer:&nbsp;{developer}</div>   
-                            <div className='text-font'>Released:&nbsp;<Moment format="DD/MM/YYYY">{relDate}</Moment></div>
+                            <div className='fontSize'>Released:&nbsp;<Moment format="DD/MM/YYYY">{relDate}</Moment></div>
                             <div className='genres'>Genres:&nbsp;{genre}</div> 
                             <div className='platforms'> Platforms:&nbsp;{platform}</div>
-                            <div className='text-font'>Metacritic score: {metacritic}</div>
-                            <div className='text-font'>Link: <a  className='website' href={website}>View the website</a></div>
+                            <div className='fontSize'>Metacritic score: {metacritic}</div>
+                            <div className='fontSize'>Link: <a  className='website' href={website}>View the website</a></div>
                         </div>
                     </div>
 
